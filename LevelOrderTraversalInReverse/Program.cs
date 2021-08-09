a829472a104dcec9ee5ee558876fa0c89e3b87fc
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-08 23:43:17 -0700

    Add project files.

---
+++ LevelOrderTraversalInReverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LevelOrderTraversalInReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(10);
+            tree.left = new Node(20);
+            tree.left.left = new Node(40);
+            tree.left.right = new Node(50);
+            tree.left.right.left = new Node(70);
+            tree.left.right.right = new Node(800);
+            tree.right = new Node(30);
+            tree.right.right = new Node(60);
+            tree.right.right.left = new Node(90);
+
+            TraversalInReverse TIR = new TraversalInReverse();
+            Console.WriteLine(string.Join(" , " , TIR.InReverse(tree)));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LevelOrderTraversalInReverse/TraversalInReverse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelOrderTraversalInReverse
+{
+    class TraversalInReverse
+    {
+        public Stack<int> InReverse(Node root)
+        {
+            Queue<Node> Q = new Queue<Node>();
+            Stack<int> S = new Stack<int>();
+            Q.Enqueue(root);
+
+            while(Q.Count != 0)
+            {
+                root = Q.Dequeue();
+                S.Push(root.data);
+                if(root.right != null)
+                {
+                    Q.Enqueue(root.right);
+                }
+
+                if (root.left != null)
+                {
+                    Q.Enqueue(root.left);
+                }
+            }
+            return S;
+
+        }
+
+    }
+}
